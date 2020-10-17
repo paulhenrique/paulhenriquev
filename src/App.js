53f7cad63f6b721825cadde67dc9899225f68bbb
Paulo Henrique <phvcandido@gmail.com>
2020-10-16 23:11:31 -0300

    Adicionando Graphic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
+import Graphic from './components/Graphic';
+
 import './assets/style/global.css';
 function App() {
   return (
     <body>
       <Navbar />
       <Header/>
+      <Graphic />
     </body>
   );
 }
---
+++ src/components/Graphic/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './style.css';
+import card1 from '../../assets/image/img1.png';
+
+function Graphic(){
+    return (
+        <div id="graphic">
+            <header>
+                <h1>Graphic Works</h1>
+            </header>
+            <section class="container-items">
+                <div class="card-item">
+                    <h3 class="card-title">Projeto Cada Gota Vale</h3>
+                    <img src={card1} alt="Cada Gota Vale" class="card-image"/>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Graphic;
