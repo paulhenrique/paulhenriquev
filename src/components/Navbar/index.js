6daed2eafb97335678ba5fa6ecc15582abff0978
Paulo Henrique <phvcandido@gmail.com>
2020-10-17 15:35:06 -0300

    Atualizando estilo e adicionando efeitos de scroll

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from "react-scroll";
+
 import './style.css';
 
 function Navbar() {
@@ -10,16 +12,36 @@ function Navbar() {
             <section>
                 <ul class="navbar-items">
                     <li class="navbar-item">
-                        <a href="#header">Início</a>
+                        <Link
+                            to="header"
+                            spy={true}
+                            smooth={true}
+                            
+                            duration={500}>Inicio</Link>
                     </li>
                     <li class="navbar-item">
-                        <a href="#graphic">Graphic</a>
+                    <Link
+                            to="graphic"
+                            spy={true}
+                            smooth={true}
+                            
+                            duration={500}>Graphic</Link>
                     </li>
                     <li class="navbar-item">
-                        <a href="#webapps">Webapps</a>
+                    <Link
+                            to="webapps"
+                            spy={true}
+                            smooth={true}
+                            
+                            duration={500}>WebApps</Link>
                     </li>
                     <li class="navbar-item">
-                        <a href="#contato">Contato</a>
+                    <Link
+                            to="footer"
+                            spy={true}
+                            smooth={true}
+                            offset={700}
+                            duration={500}>Contato</Link>
                     </li>
                 </ul>
             </section>
