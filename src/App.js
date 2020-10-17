bbcbb5041c9ffb0879c96a87f0a98b73d051b4fe
Paulo Henrique <phvcandido@gmail.com>
2020-10-17 10:54:36 -0300

    Adicionando webapp

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Graphic from './components/Graphic';
+import WebApps from './components/webApps';
 
 import './assets/style/global.css';
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar/>
       <Header/>
       <Graphic />
+      <WebApps />
     </body>
   );
 }
---
+++ src/components/webApps/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Card from '../card';
+import './style.css';
+import card1 from '../../assets/image/card1.png';
+import card2 from '../../assets/image/card2.png';
+import card3 from '../../assets/image/card3.png';
+import card4 from '../../assets/image/card4.png';
+import card5 from '../../assets/image/card5.png';
+
+
+function WebApps() {
+    return (
+        <div id="webapps">
+            <header>
+                <h1>Web Apps</h1>
+            </header>
+
+            <section class="container-items">
+                <Card cardImage={card1} cardTitle="Projeto Cada Gota Vale" />
+                <Card cardImage={card2} cardTitle="IFSP Campus Itapetininga" />
+                <Card cardImage={card3} cardTitle="Prefeitura de Itapetininga" />
+                <Card cardImage={card4} cardTitle="OBPC Itapetininga" />
+                <Card cardImage={card5} cardTitle="Hello Yotta" />
+            </section>
+        </div>
+    )
+}
+
+export default WebApps;
