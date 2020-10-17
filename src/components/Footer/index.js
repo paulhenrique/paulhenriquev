3f7fb369a2edfa6f31d62c365d4a0f6306594cf1
Paulo Henrique <phvcandido@gmail.com>
2020-10-17 15:50:21 -0300

    Estilizando e configurando links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -12,16 +12,16 @@ function Footer() {
                 <div class="social_links">
                     <ul>
                         <li>
-                            <a href="https://www.google.com/">Facebook</a>
+                            <a target="_blank" href="https://www.facebook.com/paulhenriquev/">Facebook</a>
                         </li>
                         <li>
-                            <a href="https://www.google.com/">Email</a>
+                            <a target="_blank" href="mailto:phvcandido@gmail.com">Email</a>
                         </li>
                         <li>
-                            <a href="https://www.google.com/">Whatsapp</a>
+                            <a target="_blank" href="https://api.whatsapp.com/send?phone=5515997088134&text=Ol%C3%A1%2C%20Paulo!%20Estou%20entrando%20em%20contato%20atrav%C3%A9s%20do%20seu%20site!">Whatsapp</a>
                         </li>
                         <li>
-                            <a href="https://www.google.com/">Github</a>
+                            <a target="_blank" href="https://github.com/paulhenrique">Github</a>
                         </li>
                     </ul>
                 </div>
